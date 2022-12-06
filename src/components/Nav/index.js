34abc7b550b705767f8ee727b0d334764b180e8c
vpratnicki <vanessa@pratnicki.net>
2022-12-05 22:02:27 -0500

    Chore: About and Nav components setup

---
+++ src/components/Nav/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { capitalizeFirstLetter } from '../../utils/helpers'
+
+
+function Nav() {
+    const categories = [
+        { name: "commercial", description: "Photos of grocery stores, food trucks, and other commercial projects" },
+        { name: "portraits", description: "Portraits of people in my life" },
+        { name: "food", description: "Delicious delicacies" },
+        { name: "landscape", description: "Fields, farmhouses, waterfalls, and the beauty of nature" },
+      ];
+
+    function categorySelected(name) {
+        console.log(`${name} clicked`);
+      }
+  
+      return (
+    <header data-testid="header" className="flex-row px-1">
+      <h2>
+      <a href="/">
+          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+        </a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a href="#about">About me</a>
+          </li>
+          <li className="mx-2">
+            <span>Contact</span>
+          </li>
+          {categories.map((category) => (
+            <li className="mx-1" key={category.name}>
+              <span onClick={categorySelected}>{capitalizeFirstLetter(category.name)}</span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
