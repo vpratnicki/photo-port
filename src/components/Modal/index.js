f6e6496bf21edde1fa3be87e83e2f2caec1e81b2
vpratnicki <vanessa@pratnicki.net>
2022-12-10 16:35:59 -0500

    Chore: tested Modals

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 
-function Modal({currentPhoto}) {
+const Modal = ({ onClose, currentPhoto }) => {
     const {name, category, description, index} = currentPhoto;
+    
+
 
   return (
     <div className="modalBackdrop">
       <div className="modalContainer">
-        <h3 className="modalTitle">Photo Name</h3>
+        <h3 className="modalTitle">{name}</h3>
         <img src={require(`../../assets/large/${category}/${index}.jpg`)} alt="current category" />
-        <p>Photo Description</p>
-        <button type="button">Close this modal</button>
+        <p>{description}</p>
+        <button onClick={onClose} type="button">Close this modal</button>
       </div>
     </div>
   );
--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -110,13 +110,15 @@ const PhotoList = ({ category }) => {
       const toggleModal = (image, i) => {
         // current photo
         setCurrentPhoto({...image, index: i});
-        setIsModalOpen(true);
+        setIsModalOpen(!isModalOpen);
       }
 
 
       return (
         <div>
-          {isModalOpen && <Modal currentPhoto={currentPhoto} />}
+          {isModalOpen && (
+          <Modal currentPhoto={currentPhoto} onClose={toggleModal} />
+          )}
 
           <div className="flex-row">
             {currentPhotos.map((image, i) => (
