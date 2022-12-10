6fa39684377cc084d4188bff15614f80d51227ff
vpratnicki <vanessa@pratnicki.net>
2022-12-10 14:22:42 -0500

    Chore: submit the form data.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import ConfactForm from './components/Contact';
 
 
 function App() {
@@ -27,8 +28,9 @@ function App() {
       />
       <main>
         <div>
-        <Gallery currentCategory={currentCategory} />
-        <About />
+          <ConfactForm />
+          <Gallery currentCategory={currentCategory} />
+          <About />
         </div>
       </main>
     </div>
