c7a809d2ea3520f796c58b1f39cd5142dd869504
vpratnicki <vanessa@pratnicki.net>
2022-12-10 16:23:42 -0500

    Chore: created modal & managed state

---
+++ src/components/Modal/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Modal({currentPhoto}) {
+    const {name, category, description, index} = currentPhoto;
+
+  return (
+    <div className="modalBackdrop">
+      <div className="modalContainer">
+        <h3 className="modalTitle">Photo Name</h3>
+        <img src={require(`../../assets/large/${category}/${index}.jpg`)} alt="current category" />
+        <p>Photo Description</p>
+        <button type="button">Close this modal</button>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import Modal from '../Modal';
+
 
 
 const PhotoList = ({ category }) => {
@@ -101,16 +103,30 @@ const PhotoList = ({ category }) => {
       ]);
     
       const currentPhotos = photos.filter(photo => photo.category === category);
+      const [currentPhoto, setCurrentPhoto] = useState();
+      const [isModalOpen, setIsModalOpen] = useState(false);
+
+
+      const toggleModal = (image, i) => {
+        // current photo
+        setCurrentPhoto({...image, index: i});
+        setIsModalOpen(true);
+      }
+
+
       return (
         <div>
+          {isModalOpen && <Modal currentPhoto={currentPhoto} />}
+
           <div className="flex-row">
             {currentPhotos.map((image, i) => (
               <img
-                src={require(`../../assets/small/${category}/${i}.jpg`).default}
-                alt={image.name}
-                className="img-thumbnail mx-1"
-                key={image.name}
-              />
+              src={require(`../../assets/small/${category}/${i}.jpg`)}
+              alt={image.name}
+              className="img-thumbnail mx-1"
+              onClick={() => toggleModal(image, i)}
+              key={image.name}
+            />
             ))}
           </div>
         </div>
